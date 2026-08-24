21a66c31eb45f224bc70e99f2186f2acfbf43ad6
Alessio Gravili <github@gravili.net>
2026-08-23 19:34:17 -0700

    feat: add Payload data CLI commands

--- packages/payload/src/cli/index.spec.ts
+++ packages/payload/src/cli/index.spec.ts
@@ -60,6 +60,7 @@ describe('createCLI', () => {
 
     expect(config.cli && config.cli.commands).toMatchObject({
       build: 'payload/cli/builtin#createBuildCommand',
+      createDocuments: 'payload/cli/builtin#createCreateDocumentsCommand',
       info: 'payload/cli/builtin#createInfoCommand',
     })
   })
